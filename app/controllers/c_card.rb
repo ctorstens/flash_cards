29cfea8a1f40cf96799cccc08e6940005c581249
DBC Apprentice <apprentice@devbootcamp.com>
2013-04-27 16:13:11 -0700

    game controller game erb

--- app/controllers/c_card.rb
+++ app/controllers/c_card.rb
@@ -1,51 +1,51 @@
  
-# ######################################################
-# # CREATE
-# ######################################################
-# get '/card/create_card' do
-#   "This is where you go to create a card"
-#   erb :create_card
-# end
+######################################################
+# CREATE
+######################################################
+get '/card/create_card' do
+  "This is where you go to create a card"
+  erb :create_card
+end
 
-# post '/card/create_card' do
-#   "This is where you create a card"
-#   Card.create(params[:question], params[:answer])
-#   erb :card_profile
-# end
+post '/card/create_card' do
+  "This is where you create a card"
+  Card.create(params[:question], params[:answer])
+  erb :card_profile
+end
 
-# ######################################################
-# # READ
-# ######################################################
-# get '/card/:id' do
-#   "This is where you will view a card"
-#   @card = Card.find(params)
-#   erb :card_profile
-# end
+######################################################
+# READ
+######################################################
+get '/card/:id' do
+  "This is where you will view a card"
+  @card = Card.find(params)
+  erb :card_profile
+end
 
 
-# ######################################################
-# # UPDATE
-# ######################################################
+######################################################
+# UPDATE
+######################################################
 
-# get '/card/:id/edit' do
-# "this page, you'll edit a card"
-#   erb :edit_card ## or create card???
-# end
+get '/card/:id/edit' do
+"this page, you'll edit a card"
+  erb :edit_card ## or create card???
+end
 
-# post 'card/:id/edit' do
-#   @card = Card.find(params[:id])
-#   @card.update_attributes(params[:answer], params[:question])
-#   erb :card_profile
-# end
+post 'card/:id/edit' do
+  @card = Card.find(params[:id])
+  @card.update_attributes(params[:answer], params[:question])
+  erb :card_profile
+end
 
-# ######################################################
-# # DESTROY
-# ######################################################
+######################################################
+# DESTROY
+######################################################
 
-# delete '/card/:id' do
-#   @card = Resource.find(params[:id])
-#   @card.destroy
-#   erb :deck_profile
-# end
+delete '/card/:id' do
+  @card = Card.find(params[:id])
+  @card.destroy
+  erb :card_profile
+end
 
 
