59f05dcd67f2c5eb2ffe36dff288d9d8655a4d94
Chris Torstenson <itunes.torstenson@gmail.com>
2013-04-26 17:57:18 -0700

    changed seed password to '1234' for every fake user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ require 'faker'
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
-    password: Faker::Lorem.word
+    password: 1234
     })
 end
 users = User.all
