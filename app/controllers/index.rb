d0347f647e73a860508e7611112cf8952c62f793
Apprentice <apprentice@devbootcamp.com>
2013-04-26 19:37:50 -0700

    added bootstrap and logo to erbs

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,8 +1,8 @@
-get '/resources' do
+get '/' do
   # Typically the index of a resource returns a collection of the resources
   # For instances, a blog may have a '/' route that returns all the articles
   # @resources = Resource.all
-  # ...
+  erb :index
 end
 
 get '/resources/:id' do
