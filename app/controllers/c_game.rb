8648f0d9c5630966409f1e1327e16bf6073aa82f
Kristine Hines <Hines.kristine@gmail.com>
2013-04-28 12:18:19 -0700

    game controller

--- app/controllers/c_game.rb
+++ app/controllers/c_game.rb
@@ -4,7 +4,7 @@ end
 
 
 post '/play_deck' do
-  @current_round = Round.create(deck_id: 1, user_id: 1, card_id: 1, count_correct: 0, count_wrong: 0 )
+  @current_round = Round.create(deck_id: 1, user_id: 1, card_id: 2, count_correct: 0, count_wrong: 0 )
   @current_card = Card.find(@current_round.card_id)
   @current_question = @current_card.question
   erb :play
