5c8161e55d2cc51cabce7361fbc1e25138740f82
Apprentice <apprentice@devbootcamp.com>
2013-04-27 19:14:38 -0700

    enable logout

--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,12 +1,16 @@
 helpers do
   def current_user
-    @user ||= User.find_by_token(session[:token])
-    # User.find_by_token(session[:token])
+    # @user ||= User.find_by_token(session[:token])
+    User.find_by_token(session[:token])
   end
 
   def get_token
-    user_pw_hash = User.find_by_token(session[:token]).password_hash
-    the_seed = user_pw_hash + Time.now.to_s + "kjsd94*asdHH*&h80fh"
+    # @user = current_user
+    # user_pw_hash = @user.password_hash
+    # the_seed = user_pw_hash + Time.now.to_s + "kjsd94*asdHH*&h80fh"
+    # Digest::MD5.hexdigest(the_seed)
+
+    the_seed = Time.now.to_s + "kjsd94*asdHH*&h80fh"
     Digest::MD5.hexdigest(the_seed)
   end
 end
