4bd5e22244a302ad1b9f1654801334ceb7b6415e
Apprentice <apprentice@devbootcamp.com>
2013-04-27 19:15:22 -0700

    enable pass-through-home if user logged in

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,5 +3,11 @@ get '/' do
   # Typically the index of a resource returns a collection of the resources
   # For instances, a blog may have a '/' route that returns all the articles
   # @resources = Resource.all
-  erb :index
+
+  # erg :user_profile unless current_user == nil
+  if current_user
+    erb :user_profile
+  else
+    erb :index
+  end
 end
